8cd1258e6bc9a197802949c01f25c8689e34c2d4
John Burton <71100259+johnburton0517@users.noreply.github.com>
2023-01-13 13:40:31 -0600

    trying to append previous command

--- script.js
+++ script.js
@@ -3,45 +3,74 @@
 const input = document.getElementById("terminal-input");
 const output = document.getElementById("terminal-output");
 const prevCommand = document.getElementById("prev-command");
+const welcome = document.getElementById("welcome");
+
 
 // gets the input from the user for the terminal
 input.addEventListener("keyup", function(event) {
     // 13 is the keycode for the enter key
     if (event.keyCode === 13) {
-        const text = input.value;
+        const text = input.value.toLowerCase();
         
         // outputs the previous command
-        // output.innerHTML += "user@terminal: " + text + "<br>";
-        prevCommand.innerHTML = "user@terminal:~$ " + text + "<br>";
+        // prevCommand.innerHTML = "user@terminal:~$ " + text + "<br>";
 
-        // // if user inputs "help"
-        if (text === "help" || text === "Help" || text === "HELP") {
-            output.innterHTML += helpInput();
-        }
-        // if user inputs "clear"
-        else if (text === "clear" || text === "Clear" || text === "CLEAR") {
-            output.innerHTML = clearInput();
-        }
+        // printPrevCommand(text)
 
         // // switch case for input
-        // switch (text) {
-        //     case help:
-        //         helpInput()
-        // }
+        switch (String(text)) {
+            case "help":
+                printPrevCommand(text)
+                helpInput();
+                break;
+
+            case "clear":
+                clearInput();
+                break;
+
+            case "about":
+                aboutInput();
+                break;
+
+            default:
+                //TODO: change color of help
+                wrongInput();
+        }
 
         // Clear the input field
-        input.value = "";  
+        input.value = "";
     }
 });
 
 // if user inputs "help"
 function helpInput() {
-    return output.innerHTML += "about<br>contact<br>projects<br>skills<br>education<br>resume<br>clear<br>";
+    output.innerHTML += "about<br>contact<br>projects<br>skills<br>education<br>resume<br>clear<br>";
+
+}
+
+// TODO
+// if user inputs "about"
+function aboutInput() {
+    output.innerHTML += "about";
 }
 
 // if user inputs "clear"
 function clearInput() {
     prevCommand.innerHTML = "";
-    return output.innerHTML = "";
+    welcome.innerHTML = "";
+    output.innerHTML = "";
 }
-  
+
+function wrongInput() {
+    output.innerHTML += "Command not found. For a list of commands, type 'help'<br>";
+}
+
+function printPrevCommand(text) {
+    var command = document.createElement("h3");
+    command.className = ".prev-command";
+
+    command.innerHTML = "user@terminal:~$ " + text + "<br>";
+    command.appendChild(command);
+    var terminalOutput = document.getElementById("terminal-output");
+    terminalOutput.insertBefore(command, terminalOutput);
+}
