52199c0a09f7e7e82e39415ed9998dd4b387598d
John Burton <jwburton@email.sc.edu>
2023-01-13 14:36:45 -0600

    up and down arrows for commands works

--- script.js
+++ script.js
@@ -2,25 +2,28 @@
 
 const input = document.getElementById("terminal-input");
 const output = document.getElementById("terminal-output");
-const prevCommand = document.getElementById("prev-command");
 const welcome = document.getElementById("welcome");
+
+// array to store previous commands
+let prevCommands = [];
+let prevCommandsIndex = 0;
 
 
 // gets the input from the user for the terminal
 input.addEventListener("keyup", function(event) {
     // 13 is the keycode for the enter key
     if (event.keyCode === 13) {
-        const text = input.value.toLowerCase();
-        
-        // outputs the previous command
-        // prevCommand.innerHTML = "user@terminal:~$ " + text + "<br>";
+        // store the previous command
+        prevCommands.push(input.value);
+        prevCommandsIndex = prevCommands.length;
 
-        // printPrevCommand(text)
+        // get the input from the user
+        const text = input.value.toLowerCase();
 
         // // switch case for input
         switch (String(text)) {
             case "help":
-                printPrevCommand(text)
+                printPrevCommand(text);
                 helpInput();
                 break;
 
@@ -29,11 +32,13 @@ input.addEventListener("keyup", function(event) {
                 break;
 
             case "about":
+                printPrevCommand(text);
                 aboutInput();
                 break;
 
             default:
                 //TODO: change color of help
+                printPrevCommand(text);
                 wrongInput();
         }
 
@@ -42,10 +47,46 @@ input.addEventListener("keyup", function(event) {
     }
 });
 
+
+// if use presses up arrow, gets previous command
+input.addEventListener("keydown", function(event) {
+    // 38 is the keycode for the up arrow
+    if (event.keyCode === 38) {
+        // if there are no previous commands, do nothing
+        if (prevCommandsIndex === 0) {
+            return;
+        }
+        else {
+            input.value = prevCommands[prevCommandsIndex - 1];
+            prevCommandsIndex--;
+        }
+
+        // move cursor to end of input
+        input.selectionStart = input.selectionEnd = input.value.length;
+    }
+    // 40 is the keycode for the down arrow
+    else if (event.keyCode === 40) {
+        // if there are no previous commands, do nothing
+        if (prevCommands[prevCommandsIndex + 1] === undefined) {
+            return;
+        }
+        else {
+            input.value = prevCommands[prevCommandsIndex + 1];
+            prevCommandsIndex++;
+        }
+
+        // move cursor to end of input
+        input.selectionStart = input.selectionEnd = input.value.length;
+    }
+});
+
+
+
+
+
 // if user inputs "help"
 function helpInput() {
     output.innerHTML += "about<br>contact<br>projects<br>skills<br>education<br>resume<br>clear<br>";
-
 }
 
 // TODO
@@ -56,21 +97,14 @@ function aboutInput() {
 
 // if user inputs "clear"
 function clearInput() {
-    prevCommand.innerHTML = "";
     welcome.innerHTML = "";
     output.innerHTML = "";
 }
 
 function wrongInput() {
-    output.innerHTML += "Command not found. For a list of commands, type 'help'<br>";
+    output.innerHTML += "Command not found. For a list of commands, type <span class='command-text'>'help'</span><br>";
 }
 
 function printPrevCommand(text) {
-    var command = document.createElement("h3");
-    command.className = ".prev-command";
-
-    command.innerHTML = "user@terminal:~$ " + text + "<br>";
-    command.appendChild(command);
-    var terminalOutput = document.getElementById("terminal-output");
-    terminalOutput.insertBefore(command, terminalOutput);
+    output.innerHTML += `<h3 class='prev-command'>user@terminal:~$ ${text}<br></h3>`;
 }
